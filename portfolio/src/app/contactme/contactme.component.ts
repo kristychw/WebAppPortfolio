222dde7bc0c5d02934d11031546a5db78b8cce7e
kristychw <hchan133@my.centennialcollege.ca>
2023-06-11 14:53:43 -0400

    revised on 11Jun

--- portfolio/src/app/contactme/contactme.component.ts
+++ portfolio/src/app/contactme/contactme.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contactme',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactme.component.css']
 })
 export class ContactmeComponent {
+  constructor(private router: Router) {}
 
+  contactForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    contactNumber: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+  });
+
+  onSubmitBtn() {
+    if (this.contactForm.valid) {
+      console.warn(this.contactForm.value);
+
+      // Navigate to the '/home' route after form submission
+      this.router.navigate(['/home']);
+    }
+  }
 }
